015f8146e39beff72bc2b4bdef8ea284c6619467
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2018-11-29 03:04:02 -0500

    basic plots created and little dive into the data

--- Trolls.R
+++ Trolls.R
@@ -20,6 +20,11 @@ library(tidyverse)
 library(dplyr)
 library(fs)
 library(stringr)
+library(ggplot2)
+library(lubridate)
+
+# Building a not in function
+`%!in%` = Negate(`%in%`)
 
 # THE FOLLOWING IS FOR THE INITIAL FILE DOWNLOAD OF MY RUSSIAN 
 # TROLL DATA. ALL OF THE DATA IS PRESENT ON MY LOCAL MACHINE.
@@ -48,3 +53,45 @@ library(stringr)
 
 # Reading in the data
 trolls <- read_rds("trolls.rds")
+
+# Selecting only relevant columns
+troll_clean <- trolls %>% 
+  select(author, language, content, followers, following,
+         account_type, account_category, post_type, publish_date, updates,  
+         external_author_id, region) %>% 
+  mutate(publish_date = mdy_hm(publish_date))
+
+# Taking a closer look
+troll_clean %>% 
+  count(language, account_category) %>% 
+  filter(account_category == "Unknown") %>% 
+  View()
+
+# Found that however language was determined seems to be a pretty 
+# unreliable source of data. Checked out results from "Albanian"
+# and "LANGUAGE UNDEFINED" to reach this conclusion.
+trolls %>% 
+  #filter(language == "LANGUAGE UNDEFINED") %>%
+  filter(language == "Albanian") %>% 
+  View()
+
+# Seeing if there were any repeat tweets from those associated 
+# with the agency
+trolls %>%
+  filter(post_type %!in% c("RETWEET", "QUOTE_TWEET")) %>% 
+  count(content, post_type) %>%
+  arrange(desc(n)) %>% 
+  top_n(20) %>%  View()
+
+# Plotting the number of active twitter accounts associated with trolls
+# over time
+troll_clean %>% 
+  count(quarter_date = quarter(publish_date, with_year = TRUE), author) %>% 
+  count(quarter_date) %>% 
+  ggplot(aes(x = quarter_date, y = nn)) + geom_point() + geom_smooth() + scale_y_log10()
+
+# Plotting the use of the term fake news. First tweeted by TRUMP in January 2017
+troll_clean %>% 
+  filter(str_detect(tolower(content), ("fake news|fakenews"))) %>% 
+  count(quarter_date = quarter(publish_date, with_year = TRUE)) %>% 
+  ggplot(aes(x = quarter_date, y = n)) + geom_point() + geom_smooth() + scale_y_log10() 
