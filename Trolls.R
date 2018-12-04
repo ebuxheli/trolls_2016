a4e357c218edfcb878af65a6e52eb7267781feb6
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2018-12-03 22:24:20 -0500

    added comments and daily troll postings

--- Trolls.R
+++ Trolls.R
@@ -12,7 +12,9 @@
 # Twitter. I will look to see if there is any indication of trends amongst the 
 # different types of trolls on Twitter created for this Presidential Election.
 # 
-# mru: Enxhi Buxheli 11/29/2018 - plots created and data dive
+# mru: Enxhi Buxheli 12/3/2018 
+#   + added comments to data
+#   + narrowed down selected variables
 
 
 # Attaching libraries
@@ -23,8 +25,13 @@ library(stringr)
 library(ggplot2)
 library(lubridate)
 
-# Building a not in function
+
+# Building a "not in" function to poke around the data.
+# You may or may not see it in use in this version of the project
+# as I deleted some ad hoc data dives.
+
 `%!in%` = Negate(`%in%`)
+
 
 # THE FOLLOWING IS FOR THE INITIAL FILE DOWNLOAD OF MY RUSSIAN 
 # TROLL DATA. ALL OF THE DATA IS PRESENT ON MY LOCAL MACHINE.
@@ -39,7 +46,6 @@ library(lubridate)
 # NOTE: Even advanced compression techniques couldn't bring the
 # rds file size below 100MB for GitHub uploading.
 #
-#
 # ### Consolidating troll data 
 # # Listing all of the file names for troll data
 # files_names <- dir_ls(path = "troll_data/")
@@ -51,47 +57,59 @@ library(lubridate)
 # # joining prompts again by storing output into an rds file
 # write_rds(troll_data, "trolls.rds")
 
-# Reading in the data
+
+# Reading in the cleaned up data to avoid crazy run times for the above
+# lines of code that process the data for analytical use.
 trolls <- read_rds("trolls.rds")
 
-# Selecting only relevant columns
+
+# Here I have selected the columns that I thought were relevant for this project.
+# I wanted to do a deeper dive into who the authors were, the content of their 
+# tweets to see if I could find any interesting trends in some specific phrases
+# or words, along with the impact that they have shown by the followers and following
+# metrics, publish date to see the timestamp of these tweets and do more a sequential
+# analysis. The rest of the variables I kept just in case I wanted to take a look at 
+# these variables in the future (I have not used them thus far).
+#
+# I took a deeper look at variables from trolls to select and analyze and found some 
+# to be unreliable. Found that however language was determined seems to be a pretty 
+# unreliable source of data. I checked out results from "Albanian" and "LANGUAGE 
+# UNDEFINED" to reach this conclusion and found that many of these tweets were in 
+# English. Also looked to see if there were any repeat tweets from those associated 
+# with the agency (IRA) and found that post_type was also not a reliable metric 
+# because it seems that it didn't count many of the quoted and retweeted tweets.
+#
+# Considering to create an rds file that is hopefully smaller than the trolls.rds
+# file to upload to Github for replicability. Otherwise will have a download.file
+# statement here for the file linked from the Google Drive.
+
 troll_clean <- trolls %>% 
-  select(author, language, content, followers, following,
-         account_type, account_category, post_type, publish_date, updates,  
+  select(author, content, followers, following,
+         account_type, account_category, publish_date, updates,  
          external_author_id, region) %>% 
   mutate(publish_date = mdy_hm(publish_date))
 
-# Taking a closer look
-troll_clean %>% 
-  count(language, account_category) %>% 
-  filter(account_category == "Unknown") %>% 
-  View()
 
-# Found that however language was determined seems to be a pretty 
-# unreliable source of data. Checked out results from "Albanian"
-# and "LANGUAGE UNDEFINED" to reach this conclusion.
-trolls %>% 
-  #filter(language == "LANGUAGE UNDEFINED") %>%
-  filter(language == "Albanian") %>% 
-  View()
+# Plotting the number of active Twitter accounts associated with trolls
+# over time to observe some of the trends in number of active accounts
+# and times when Twitter was removing some of the accounts.
 
-# Seeing if there were any repeat tweets from those associated 
-# with the agency
-trolls %>%
-  filter(post_type %!in% c("RETWEET", "QUOTE_TWEET")) %>% 
-  count(content, post_type) %>%
-  arrange(desc(n)) %>% 
-  top_n(20) %>%  View()
-
-# Plotting the number of active twitter accounts associated with trolls
-# over time
 troll_clean %>% 
   count(quarter_date = quarter(publish_date, with_year = TRUE), author) %>% 
   count(quarter_date) %>% 
   ggplot(aes(x = quarter_date, y = nn)) + geom_point() + geom_smooth() + scale_y_log10()
 
-# Plotting the use of the term fake news. First tweeted by TRUMP in January 2017
+# Plotting the use of the term fake news. First tweeted by TRUMP in January 2017.
 troll_clean %>% 
   filter(str_detect(tolower(content), ("fake news|fakenews"))) %>% 
   count(quarter_date = quarter(publish_date, with_year = TRUE)) %>% 
   ggplot(aes(x = quarter_date, y = n)) + geom_point() + geom_smooth() + scale_y_log10() 
+
+## Replicating some of the graphs found in the FiveThirtyEight article.
+## https://fivethirtyeight.com/features/why-were-sharing-3-million-russian-troll-tweets/
+troll_clean %>%
+  count(day_of = as.Date(publish_date, format = "%d")) %>%
+  ggplot(aes(x = day_of, y = n)) + 
+    geom_col()
+  
+  
