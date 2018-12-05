6e7fa999c8df0e216734d78d8d539c33ddb69274
Enxhi Buxheli <enxhibuxheli@Enxhis-MacBook-Pro.local>
2018-12-05 03:06:38 -0500

    first shiny commit

---
+++ trolls_2016/app.R
@@ -0,0 +1,60 @@
+# Russian Trolls, App
+# 
+# By: Enxhi Buxheli 
+# Date: December 4th, 2018
+# 
+# --- Credit to FiveThirtyEight and ClemsonU for the data ---
+#   
+#   This is a shiny app that presents Russian Troll data from the timeframe relevant 
+# for the 2016 US Presidential Election which I processed to be displayed here. This
+# app displays plots that help tell some of the interesting stories this data holds.
+# 
+# mru: Enxhi Buxheli 12/5/2018 
+#   + created app
+
+
+library(shiny)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
